b5035c41918d73efe6a971b7208fcaf7a2706a32
kabishsridar <kabishsridar6@gmail.com>
2025-02-28 16:01:38 +0530

    Ex_1.py

--- Ex_1.py
+++ Ex_1.py
@@ -1,6 +1,5 @@
-#This is Exercises 1
-
+#This is Exercise no.1
 print("exercise1")
+#the print function returns the variables or text or numbers we added inside the doble quotes for example:
 print("my name is kabish")
 print("this is my first programme")
-#the print function prints the variables or text or numbers inside the doble quotes
