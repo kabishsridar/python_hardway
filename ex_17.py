12fa7024a75b5a06ea454504962cf8770520cbd9
kabishsridar <kabishsridar6@gmail.com>
2025-03-14 22:14:04 +0530

    ex_17.py

--- ex_17.py
+++ ex_17.py
@@ -1,13 +1,13 @@
 from sys import argv
 from os.path import exists
 #now assigning the arguement variables
-script, from_file,to_file=argv,argv,argv
+script, from_file, to_file=argv
 
 #introducing the variables to print funct, by f
 print(f"copying from {from_file} to {to_file}")
 
 #doing this in one line
-in_file = open(from_file)
+in_file = open("text.txt")
 indata = in_file.read()
 
 print(f"the input file is {len(indata)} bytes long")
@@ -24,3 +24,4 @@ print("alright,all done.")
 out_file.close()
 in_file.close()
 #the file gets closed
+#to execute this programme you should open cmd and type python {this file name} {from file} {to file}
