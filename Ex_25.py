63202268cffe04d97c41d710f6e54c097b501588
kabishsridar <kabishsridar6@gmail.com>
2025-03-14 20:49:29 +0530

    Ex_25.py

--- Ex_25.py
+++ Ex_25.py
@@ -1,42 +1,52 @@
-#now introducing def funct.
-#now the word breaks up
+sentence = "All good things come to those who wait." 
+word ="Kailash"
+
 def break_words(stuff):    
-    """THis functionwill break up words for us."""
-    words  = stuff.split(' ')
-    return words
-#now sorting in alphabetic order
+    """This function will break up the words for us."""
+    words = stuff.split(" ")
+    return words  # Return the list of words
+
+# Now sorting in alphabetic order
 def sort_words(words):    
-    """sorts the words . """
+    """Sorts the words."""
     return sorted(words)
-#now printing only first word
+
+# Now printing only the first word
 def print_first_word(words):    
-    """prints the first word after popping it off."""
+    """Prints the first word after popping it off."""
     word = words.pop(0)
     print(word)
-#now printing only last word
+
+# Now printing only the last word
 def print_last_word(words):    
-    """prints the last word after popping it off."""
+    """Prints the last word after popping it off."""
     word = words.pop(-1)
     print(word)
-#now sorting sentence
+
+# Now sorting the sentence
 def sort_sentence(sentence):    
-    """Takes in a full sentence and returns the sorted words . """
+    """Takes in a full sentence and returns the sorted words."""
     words = break_words(sentence)
     return sort_words(words)
 
-def print_first_and_last(setence):    
-    """prints the first and last words of the sentence."""
+def print_first_and_last(sentence):    
+    """Prints the first and last words of the sentence."""
     words = break_words(sentence)
     print_first_word(words)
     print_last_word(words)
 
 def print_first_and_last_sorted(sentence):    
-    """sorts the wods then prints the first and last one . """
+    """Sorts the words then prints the first and last one."""
     words = sort_sentence(sentence)
     print_first_word(words)
     print_last_word(words)
 
-sentence = "All good things come to those who wait." 
-print_first_and_last_sorted(sentence)
+# Testing the functions
+words = break_words(sentence)
+print(words)
+print_first_word(words[:]) 
+print_last_word(words[:]) 
 print(sort_sentence(sentence))
-print(break_words(sentence))
+print(sort_words(break_words(word)))
+print_first_and_last_sorted(sentence)
+print_first_and_last(sentence)
