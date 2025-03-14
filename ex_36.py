81ac1a284f97041cdae4b5d43cd23c9eed9ddb3a
kabishsridar <kabishsridar6@gmail.com>
2025-03-14 23:24:43 +0530

    Ex_36.py

--- ex_36.py
+++ ex_36.py
@@ -1,38 +1,38 @@
 from sys import exit
 
 def black_thunder():
-    print("we can play every water games here.which one you want to play?")
+    print("We can play every water game here. Which one do you want to play?")
     choice = input(">")
     if choice == "anything":
-        print("you can't take a decision properly but you can enjoy yourself")
-    elif choice == "what are the games are here?":
-        print("there are many , you please search those in the official website")
+        print("You can't take a decision properly, but you can enjoy yourself!")
+    elif choice == "what are the games here?":
+        print("There are many. Please search for those on the official website.")
     elif choice == "roller coaster":
-        print("you will have a joyfull ride in the rller coaster, enjoy it")
+        print("You will have a joyful ride in the roller coaster. Enjoy it!")
     else:
-        print("you revisit again when you want to really enjoy") 
+        print("Revisit again when you want to really enjoy.")
 
 def wonderla_park():
-    print("you can see the watergames also the drift games")
-    print("enjoy the games whatever you need")
+    print("You can see the water games and drift games.")
+    print("Enjoy the games you want.")
     choice = input(">")
-    if choice == "what are the watergames avalable here?" :
-        print("swimming ,boomerang , lucky falls")
-    elif choice == "what are the land drift game?":
-        print("flash tower,equinox , maverick")
+    if choice == "water games":
+        print("Swimming, Boomerang, Lucky Falls.be happy")
+    elif choice == "land drift":
+        print("Flash Tower, Equinox, Maverick. have fun")
     else:
-        print("play anything you want")
+        print("Play anything you want.")
 
 def vac_start():
-    print("you have got a vaccational leave")
-    print("what idea you have?")
-    print("which amusement park you are going to move?")
+    print("You have got a vacation leave.")
+    print("What idea do you have?")
+    print("Which amusement park are you going to visit?")
     choice = input(">")
-    if choice ==" black thunder":
+    if choice == "black thunder":
         black_thunder()
     elif choice == "wonderla":
         wonderla_park()
-    else :
-       print(" stay in your home")
+    else:
+        print("Stay at home and relax.")
 
 vac_start()
