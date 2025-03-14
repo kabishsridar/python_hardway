0ac431fb5949f120fab30851bb2b7232402ecc21
kabishsridar <kabishsridar6@gmail.com>
2025-03-14 22:36:12 +0530

    ex_21.py

--- ex_21.py
+++ ex_21.py
@@ -1,30 +1,30 @@
 def add(a,b):    
-    print(f"adding{a}+{b}")
+    print(f"\nadding {a} + {b}")
     return a + b
 
 def subtract(a,b):    
-    print(f"subtracting{a}-{b}")
+    print(f"subtracting {a} - {b}")
     return a - b
 
 def multiply(a,b):    
-    print(f"multiplying{a}*{b}")
+    print(f"multiplying {a} * {b}")
     return a * b
 
 def divide(a,b):    
-    print(f"dividing{a}/{b}")
+    print(f"dividing {a} / {b}\n")
     return a / b
  
-print("doing some math with just the functions!")
+print("Doing some math with just the functions!")
  #now assigning math values 
-age=add(10,4)
+age=add(10,7)
 height=subtract(135,40)
 weight=multiply(50,15)
 iq=divide(150,2)
 
-print(f" Age:{age}, height: {height}, weight={weight} , iq={iq}")
+print(f"Age: {age}, height: {height}, weight= {weight} , iq= {iq}")
 
 #that puzzle,now i am typing
-print("here is a puzzle")
+print("\nhere is a puzzle\n")
 
 what = add(age,subtract(height,multiply(weight,divide(iq,2))))
 
