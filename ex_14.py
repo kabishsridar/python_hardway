200be0114c05b34390543a26f437add15ac866f4
kabishsridar <kabishsridar6@gmail.com>
2025-03-14 21:41:27 +0530

    ex_14.py

--- ex_14.py
+++ ex_14.py
@@ -1,22 +1,24 @@
 from sys import argv
-script,user_name = argv,argv
-#now assigning the prompt value
-prompt='>'
-#now introducing the varables to print
-print(f"hi {user_name},this is the {script}script.")
-print("i'd like to ask you a few questions.")
-print(f"how much do you like me {user_name}")
-#now we are inroducing input string to be answered by the readers 
 
+# Unpacking argv into variables
+script, user_name = argv
+
+# Assigning the prompt value
+prompt = '> '
+
+# Introducing the variables to print
+print(f"Hi {user_name}, this is the {script} script.")
+print("I'd like to ask you a few questions.")
+print(f"How much do you like me, {user_name}?")
+
+# Introducing input string to be answered by the readers
 likes = input(prompt)
-print("what is your name?")
-name=input(prompt)
-print("where are you from ?")
-place=input(prompt)
-#now gathering informations from answers
+print("Where are you from?")
+place = input(prompt)
 
+# Gathering information from answers
 print(f"""
-so , now you like me {likes}.
-you are from {place}. 
-and your name is {name}.
+So, now you like me {likes}.
+You are from {place}.
 """)
+#to run this program you should open command prompt and type python (this filename as saved).py (user name)
