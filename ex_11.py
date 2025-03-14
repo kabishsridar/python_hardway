cb6fe55c75cabeeca4e520960f3ba16fc935a08b
kabishsridar <kabishsridar6@gmail.com>
2025-03-14 20:13:13 +0530

    ex_11.py

--- ex_11.py
+++ ex_11.py
@@ -3,12 +3,22 @@ print("How old are you?", end=' ')
 #now commanding the input() to answer by the reader for these questions
 #now assigning the input as age
 age= input()
-print("How tall are you?", end=' ')
+print("How tall are you(cm)?", end=' ')
 #now assigning the input as height
-height = input()
-print("How much do you weigh?", end=' ')
+height = int(input())
+print("How much do you weigh(kg)?", end=' ')
 #now assigning the input as weight
-weight = input()
-
+weight = int(input())
+h = height/100
+b= h*h
+BMI= weight/b
 #printing using format variables.
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+if BMI<16:
+    print("your BMI is: ",BMI,"it is severe thickness")
+elif 18.5<BMI<25:
+    print("your BMI is: ",BMI," which is normal")
+elif BMI>25:
+    print("your BMI is: ",BMI," it is obese Try to lose your weight")
+else:
+    print(" ")
