de7b64e8039027c5eca2ca7ea307dbb019def4c8
kabishsridar <kabishsridar6@gmail.com>
2025-03-14 20:10:01 +0530

    ex_14.py

--- ex_14.py
+++ ex_14.py
@@ -1,24 +1,22 @@
-#the end= ' ' uses to continue with the next line
-print("How old are you?", end=' ')
-#now commanding the input() to answer by the reader for these questions
-#now assigning the input as age
-age= input()
-print("How tall are you(cm)?", end=' ')
-#now assigning the input as height
-height = int(input())
-print("How much do you weigh(kg)?", end=' ')
-#now assigning the input as weight
-weight = int(input())
-h = height/100
-b= h*h
-BMI= weight/b
-#printing using format variables.
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
-if BMI<16:
-    print("your BMI is: ",BMI,"it is severe thickness")
-elif 18.5<BMI<25:
-    print("your BMI is: ",BMI," which is normal")
-elif BMI>25:
-    print("your BMI is: ",BMI," it is obese Try to lose your weight")
-else:
-    print(" ")
+from sys import argv
+script,user_name = argv,argv
+#now assigning the prompt value
+prompt='>'
+#now introducing the varables to print
+print(f"hi {user_name},this is the {script}script.")
+print("i'd like to ask you a few questions.")
+print(f"how much do you like me {user_name}")
+#now we are inroducing input string to be answered by the readers 
+
+likes = input(prompt)
+print("what is your name?")
+name=input(prompt)
+print("where are you from ?")
+place=input(prompt)
+#now gathering informations from answers
+
+print(f"""
+so , now you like me {likes}.
+you are from {place}. 
+and your name is {name}.
+""")
