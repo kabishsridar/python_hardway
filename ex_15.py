1459b19a7cc4de21d004f56b3a0389e28a116e39
kabishsridar <kabishsridar6@gmail.com>
2025-03-14 21:56:32 +0530

    ex_15.py

--- ex_15.py
+++ ex_15.py
@@ -1,16 +1,17 @@
 from sys import argv
 
-script , filename = argv,argv
+script , filename = argv
 #now opening a file
 txt=open(filename)
 
 print(f"this is my file {filename}:")
 #using txt.read to read the text in the file
-print(txt.read())
+print(txt.readline())
 
 print("type the filename again:")
 file_again = input(">")
 
 txt_again = open (file_again)
-
-print(txt_again.read())
+txt_again.readline()
+print(txt_again.readline())
+#to execute the code you should open command prompt and type python {this file name}.py {another file name which to be readed}
