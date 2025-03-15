830dbc7ed50c3d7870598ddba2617eb08baad4f1
kabishsridar <kabishsridar6@gmail.com>
2025-03-15 13:49:55 +0530

    ex_37.py

---
+++ ex_37.py
@@ -0,0 +1,16 @@
+# Keywords check
+#1.Assert 
+a = 45
+b = 0
+print(a/b)
+assert b!=0,"Zero Division Error"   # we can use raise ZeroDivisionError
+
+# lambda create short anonymous function
+s = lambda y:y*y 
+print(s(3))
+
+# escape sequences
+#\b
+print("hii\b everybody")'
+
+print("welcome to \f insight builders")
